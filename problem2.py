7354e3448d681f53b74bcbded8a9dc478cc2f8ab
Matthew Yam <matthew.yam23@ycdsbk12.ca>
2021-02-23 15:17:12 +0000

    code

--- problem2.py
+++ problem2.py
@@ -1,1 +1,23 @@
+#These are the 2 variables where the user inputs the information
+one = (int(input("Side length 1: ")))
+two = (int(input("Side length 2: ")))
+three = (int(input("Side length 3: ")))
 
+#These variables determin which side length is the longest and if the figure if=s a triangle
+if one>two and three and two+three < one:
+  print("This figure is not a triangle")
+
+if two>one and three and one+three < two:
+  print("This figure is not a triangle")
+
+if three>one and two and one+two < three:
+  print("This figure is not a triangle")
+
+if one>two and three and two+three > one:
+  print("This figure is a triangle")
+
+if two>one and three and one+three > two:
+  print("This figure is a triangle")
+
+if three>one and two and one+two > three:
+  print("This figure is a triangle")
